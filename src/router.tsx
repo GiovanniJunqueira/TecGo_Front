0f2243a7f813a9c49f58703bffee5d68eb6e7db0
Oliver <oliver.otani@gmail.com>
2025-06-02 23:51:44 -0300

    login page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
-import Test from "./pages/test";
+import LoginPage from "./pages/page";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Test/>} />
+      <Route path="/" element={<LoginPage/>} />
     </Routes>
   );
 };
